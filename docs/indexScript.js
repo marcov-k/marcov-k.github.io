93c8ef153be8932f7387cc1b7a963e3a5f8ed366
Marco Vasko-Klima <marcoaklima@gmail.com>
2026-09-19 23:49:17 -0400

    Added complete Celestial-Rays project page

--- docs/indexScript.js
+++ docs/indexScript.js
@@ -35,7 +35,8 @@ const projColors = {
     "ch": [rootStyle.getPropertyValue('--ch-primary'), rootStyle.getPropertyValue('--ch-light')],
     "vec2": [rootStyle.getPropertyValue('--vec2-primary'), rootStyle.getPropertyValue('--vec2-light')],
     "cs": [rootStyle.getPropertyValue('--cs-primary'), rootStyle.getPropertyValue('--cs-light')],
-    "pm": [rootStyle.getPropertyValue('--pm-primary'), rootStyle.getPropertyValue('--pm-light')]
+    "pm": [rootStyle.getPropertyValue('--pm-primary'), rootStyle.getPropertyValue('--pm-light')],
+    "cr": [rootStyle.getPropertyValue('--cr-primary'), rootStyle.getPropertyValue('--cr-light')]
 }
 
 const targetTitleScale = 0.5;
